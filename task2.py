0d35b639349ace4c0a344f4a02311f699a3fe20e
Sashakorepin14 <146105909+Sashakorepin14@users.noreply.github.com>
2024-12-02 06:40:40 +0000

    паы

--- task2.py
+++ task2.py
@@ -1,5 +1,21 @@
 num = int(input())
 if num >= 1 and num <= 100:
     def guess_number(num):
-        print
-        if input() == 'да' or input() == 'Да':
+        n = 100
+        min = 0
+        max = n
+        if n != num:
+            print(f'Ваше число больше {(max - min)//2}')
+            if input() == 'да' or input() == 'Да':
+                min = n // 2
+                max = n
+            else:
+                min = 0
+                max = n // 2
+            return 1
+        else:
+            print(f'ваше число {n}')
+            
+        return guess_number()
+#
+guess_number(num)
